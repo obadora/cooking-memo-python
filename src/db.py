f9d3223804707b7d44d54bffe8c8aee28feb90e6
obadora <obadora0904@gmail.com>
2025-06-15 08:59:30 +0000

    登録処理の実装

--- src/db.py
+++ src/db.py
@@ -1,15 +1,28 @@
+from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from os import environ
+from dotenv import load_dotenv
+load_dotenv()
+# ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8mb4"
+ASYNC_DB_URL = URL.create(
+    drivername="mysql+aiomysql",
+    username=environ.get('DB_USER'),
+    password=environ.get('DB_PASSWORD'),
+    host="db",
+    database=environ.get('DB_NAME'),
+    query={"charset":"utf8mb4"}
+)
 
-ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
-
-async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+async_engine = create_async_engine(ASYNC_DB_URL, connect_args={
+        "charset": "utf8mb4",
+        "use_unicode": True,
+    },echo=True)
 async_session = sessionmaker(
     autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
 )
 
 Base = declarative_base()
-
 
 async def get_db():
     async with async_session() as session:
