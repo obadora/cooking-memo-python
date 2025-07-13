ed1c0828ca6c8f15d5ca55d7188239ef261cebe5
EC2 Default User <ec2-user@ip-172-31-3-111.ap-northeast-1.compute.internal>
2025-07-13 10:29:50 +0000

    Fix CORS settings for AWS deployment

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI
-from src.routers import recipe, tag, photo
 from fastapi.middleware.cors import CORSMiddleware
+from src.routers import recipe, tag, photo
 
+origins = [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "http://cooking-memo-frontend-20250713-cloudshell-user.s3-website-ap-northeast-1.amazonaws.com",
+      "https://d3fnspeoqks5i8.cloudfront.net",
+]
 
 app = FastAPI()
+
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # ReactのURL
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+      CORSMiddleware,
+      allow_origins=origins,
+      allow_credentials=True,
+      allow_methods=["*"],
+      allow_headers=["*"],
 )
+
 app.include_router(recipe.router)
 app.include_router(tag.router)
 app.include_router(photo.router)
