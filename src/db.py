381cfec5b5b51227c2c3bcbff344b13c42732475
EC2 Default User <ec2-user@ip-172-31-3-111.ap-northeast-1.compute.internal>
2025-07-26 15:23:38 +0000

    データベース接続設定追加

--- src/db.py
+++ src/db.py
@@ -4,20 +4,33 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from os import environ
 from dotenv import load_dotenv
 load_dotenv()
-# ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8mb4"
-ASYNC_DB_URL = URL.create(
-    drivername="mysql+aiomysql",
-    username=environ.get('DB_USER'),
-    password=environ.get('DB_PASSWORD'),
-    host="db",
-    database=environ.get('DB_NAME'),
-    query={"charset":"utf8mb4"}
-)
 
-async_engine = create_async_engine(ASYNC_DB_URL, connect_args={
+# DATABASE_URLから設定を取得するか、個別の環境変数を使用
+if environ.get('DATABASE_URL'):
+    ASYNC_DB_URL = environ.get('DATABASE_URL')
+else:
+    ASYNC_DB_URL = URL.create(
+        drivername="mysql+aiomysql",
+        username=environ.get('DB_USER'),
+        password=environ.get('DB_PASSWORD'),
+        host=environ.get('DB_HOST', "db"),  # 環境変数化
+        database=environ.get('DB_NAME'),
+        query={"charset":"utf8mb4"}
+    )
+
+async_engine = create_async_engine(
+    ASYNC_DB_URL,
+    connect_args={
         "charset": "utf8mb4",
         "use_unicode": True,
-    },echo=True)
+    },
+    pool_size=5,
+    max_overflow=10,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    echo=True
+)
+
 async_session = sessionmaker(
     autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
 )
